4d18ac7ac243a97eef594526072b250ca32269d3
Travis Hoki <hoki@Traviss-MacBook-Pro.local>
2015-09-08 02:28:37 -0600

    database galleries instead of hardcoding

--- api/index.php
+++ api/index.php
@@ -1,19 +1,59 @@
 <?php
-if(isset($_REQUEST['cmd']) && isset($_REQUEST['url'])){
+include 'connect_vars.php';
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_DB);
+
+if(isset($_REQUEST['cmd'])){
 	switch($_REQUEST['cmd']){
 		case 'get-gallery':
-			echo getGallery($_REQUEST['url']);
+			if(isset($_REQUEST['slug'])){
+				echo getGallery($_REQUEST['slug']);
+			}
+		break;
+		case 'get-galleries':
+			echo getGalleries();
 		break;
 	}//switch
 }
 
-function getGallery($url){
-	$url = '../galleries/'.$url;
-	$return_arr = array();
-	$img_arr = glob($url.'/*');
-	for($i = 0 ; $i < count($img_arr) ; $i++){
-		$temp_arr =  explode("../",$img_arr[$i]);
-		array_push($return_arr, $temp_arr[1]);
-	}//for
-	return json_encode($return_arr);
+function getGallery($slug){
+	//Get slug item from gallery table
+	global $conn;
+	$sql = "SELECT *
+			FROM gallery
+			WHERE slug = '".$slug."'
+			LIMIT 1";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		$obj = $result->fetch_assoc();
+
+		//Get Image names from slug directory
+		$slug = '../galleries/'.$slug;
+		$img_arr = glob($slug.'/*');
+		$obj['images'] = array();
+		for($i = 0 ; $i < count($img_arr) ; $i++){
+			$temp_arr =  explode($slug,$img_arr[$i]);
+			array_push($obj['images'], $temp_arr[1]);
+		}//for
+		return json_encode($obj);
+	}
 }//getGallery
+
+function getGalleries(){
+	global $conn;
+	$sql = "SELECT * 
+			FROM gallery
+			ORDER BY event_year DESC, event_month DESC, event_day DESC";
+	$result = $conn->query($sql);
+
+	$array = array();
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$array[] = $row;
+		}//while
+		return json_encode($array);
+	}
+
+}//getGalleries
+
+?>
--- js/main-controller.js
+++ js/main-controller.js
@@ -1,130 +1,21 @@
-var sectionItems = [
-	{
-		id: 15,
-		title: 'Disk Golf',
-		date: 'Aug 9th, 2015',
-		image: 'disk-golf.png',
-		url: 'disk-golf',
-		gallery: true
-	},
-	{
-		id: 14,
-		title: 'Travis Bday',
-		date: 'July 8th, 2015',
-		image: 'travis-bday-2015.png',
-		url: 'travis-bday-2015',
-		gallery: true
-	},
-	{
-		id: 13,
-		title: 'Camping',
-		date: 'July 2015',
-		image: 'camping.png',
-		url: 'camping',
-		gallery: true
-	},
-	{
-		id: 12,
-		title: 'Honeymoon',
-		date: 'June 2015',
-		image: 'honeymoon.png',
-		url: 'honeymoon',
-		gallery: true
-	},
-	{
-		id: 11,
-		title: 'Wedding',
-		date: 'June 6th, 2015',
-		image: 'wedding.png',
-		url: 'http://wedding.hokihappenings.com/',
-		gallery: false
-	},
-	{
-		id: 10,
-		title: 'Save the Date',
-		date: 'Dec 4th, 2014',
-		image: 'save-the-date.png',
-		url: 'http://wedding.hokihappenings.com/#/save-the-date',
-		gallery: false
-	},
-	{
-		id: 9,
-		title: 'Proposal',
-		date: 'Dec 4th, 2014',
-		image: 'proposal.png',
-		url: 'http://wedding.hokihappenings.com/#/proposal',
-		gallery: false
-	},
-	{
-		id: 8,
-		title: 'Date Night Movie In A For',
-		date: 'Dec 22, 2014',
-		image: 'date-night-movie-in-a-fort.png',
-		url: 'date-night-movie-in-a-fort',
-		gallery: true
-	},
-	{
-		id: 7,
-		title: 'High School Graduation',
-		date: 'May 2009',
-		image: 'high-school-graduation.png',
-		url: 'high-school-graduation',
-		gallery: true
-	},
-	{
-		id: 6,
-		title: 'Senior Prom',
-		date: 'May 2009',
-		image: 'prom-2009.png',
-		url: 'prom-2009',
-		gallery: true
-	},
-	{
-		id: 5,
-		title: 'Girl Prom',
-		date: '2009',
-		image: 'girl-prom.png',
-		url: 'girl-prom',
-		gallery: true
-	},
-	{
-		id: 4,
-		title: 'Preference',
-		date: '2009',
-		image: 'preference.png',
-		url: 'preference',
-		gallery: true
-	},
-	{
-		id: 3,
-		title: 'Picture Date',
-		date: '2008',
-		image: 'picture-date.png',
-		url: 'picture-date',
-		gallery: true
-	},
-	{
-		id: 2,
-		title: 'Home Coming',
-		date: '2008',
-		image: 'home-coming.png',
-		url: 'home-coming',
-		gallery: true
-	},
-	{
-		id: 1,
-		title: 'Junior Prom',
-		date: 'May 2008',
-		image: 'prom-2008.png',
-		url: 'prom-2008',
-		gallery: true
-	},
-];
 
 
 var app = angular.module('APP', ['ui.router','ngFlowGrid'])
-.controller("mainCtrl", function($scope){
-	$scope.sectionItems = sectionItems;
+.controller("mainCtrl", function($scope, $http){
+
+	$scope.init = function(){
+		$scope.getGalleries();
+	};//init
+
+	$scope.getGalleries = function(){
+		$http.get('api/?cmd=get-galleries').
+		then(function(response) {
+			$scope.sectionItems = response.data;
+		});
+	};//getGalleries
+
+	$scope.init();
+
 })
 .controller("homeCtrl", function($scope, $state){
 
@@ -133,11 +24,7 @@ var app = angular.module('APP', ['ui.router','ngFlowGrid'])
 	};//init
 
 	$scope.clickItem = function(item){
-		if(item.gallery){
-			$state.go('gallery', {id: item.id});
-		}else{
-			window.open(item.url);
-		}
+		$state.go('gallery', {slug: item.slug});
 	};//clickItem
 
 	$scope.init();
@@ -158,18 +45,9 @@ var app = angular.module('APP', ['ui.router','ngFlowGrid'])
 	};
 })
 .controller("galleryCtrl", function($scope, $stateParams, $http, gallery_images){
-	//console.log(gallery_images);
-	$scope.gallery_images = gallery_images.data;
-	console.dir($stateParams);
-	for(var i in sectionItems){
-		if(String(sectionItems[i].id) === $stateParams.id){
-			$scope.sectionInfo = sectionItems[i];
-			break;
-		}
-	}//for
+	$scope.gallery = gallery_images.data;
 })
 .config(function($stateProvider, $urlRouterProvider) {
-  //
   // For any unmatched url, redirect to /state1
   $urlRouterProvider.otherwise("/home");
   //
@@ -181,19 +59,12 @@ var app = angular.module('APP', ['ui.router','ngFlowGrid'])
       controller: "homeCtrl"
     })
     .state('gallery', {
-      url: "/gallery/:id",
+      url: "/gallery/:slug",
       templateUrl: "partials/gallery.html",
       controller: "galleryCtrl",
       resolve: {
   		gallery_images: function($stateParams, $http){
-  			var gallery_info;
-			for(var i = 0 ; i < sectionItems.length ; i++){
-				if(String(sectionItems[i].id) === String($stateParams.id)){
-					gallery_info = sectionItems[i];
-					break;
-				}
-			}//for
-			var promise = $http.get('api/?cmd=get-gallery&url='+gallery_info.url)
+			var promise = $http.get('api/?cmd=get-gallery&slug='+$stateParams.slug)
 			.then(function(data) {
 				 return data;
 			});
